6e71c784e723e920d9c4656b2e9e6f89f5f61673
Marcio <marcio.pcmonzon@gmail.com>
2020-05-02 17:27:32 -0300

    mais dtos

---
+++ Api.Domain/Dtos/User/UserDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Api.Domain.Dtos.User
+{
+    public class UserDto
+    {
+        [Required(ErrorMessage = "Nome é campo obrigatório")]
+        [StringLength(60, ErrorMessage = "Nome deve ter no máximo {1} carateceres.")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "E-mail é campo obrigatório")]
+        [EmailAddress(ErrorMessage = "E-mail inválido!")]
+        [StringLength(100, ErrorMessage = "E-mail deve ter no máximo {1} carateceres.")]
+        public string Email { get; set; }
+    }
+}
